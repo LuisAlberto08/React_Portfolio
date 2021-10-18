7c5c14a8ba0824ae44774a5b9927239144fa7bc7
Luis Martinez <luiggye@me.com>
2021-10-17 23:02:34 -0500

    Changed profile picture. Added typewriter effect to landing page. Changed the color scheme.

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,9 +1,39 @@
 import React from 'react'
 import "./intro.scss"
+import { init } from 'ityped'
+import { useEffect, useRef } from "react"
+
+
 export default function Intro() {
+
+    const textRef = useRef();
+
+    useEffect(() => {
+        init(textRef.current, {
+            showCursor: true,
+            backDelay: 1500,
+            backSpeed: 60,
+            strings: ['Web Developer', 'Graphic Designer']
+        })
+
+    }, [])
     return (
         <div className="intro" id="intro">
-
+            <div className="left">
+                <div className="imageContainer">
+                    <img src="assets/profile.png" alt="" />
+                </div>
+            </div>
+            <div className="right">
+                <div className="wrapper">
+                    <h2>Hello, I'm</h2>
+                    <h1>Luis Martinez</h1>
+                    <h3>Freelance <span ref={textRef}></span></h3>
+                </div>
+                <a href="#skills">
+                    <img src="assets/arrow.png" alt="" />
+                </a>
+            </div>
         </div>
     )
 }
